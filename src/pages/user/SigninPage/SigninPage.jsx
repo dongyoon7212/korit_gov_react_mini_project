b03fb604e3c7a5bf501a8649c2f89dc295b16d20
dongyoon7212 <dongyoon7212@naver.com>
2026-02-12 09:43:15 +0900

    Feat: commit

--- src/pages/user/SigninPage/SigninPage.jsx
+++ src/pages/user/SigninPage/SigninPage.jsx
@@ -104,7 +104,7 @@ function SigninPage() {
                             <button
                                 onClick={() =>
                                     (window.location.href =
-                                        "http://usedprice.store/oauth2/authorization/google")
+                                        "http://usedprice.store/api/oauth2/authorization/google")
                                 }>
                                 <FcGoogle />
                                 구글로 로그인
@@ -112,7 +112,7 @@ function SigninPage() {
                             <button
                                 onClick={() =>
                                     (window.location.href =
-                                        "http://usedprice.store/oauth2/authorization/naver")
+                                        "http://usedprice.store/api/oauth2/authorization/naver")
                                 }>
                                 <SiNaver />
                                 네이버로 로그인
